0063b400fdaf8927ed81547c9826818d29c096c5
joaogil06 <Joaogmonteiro06@gmail.com>
2020-09-20 12:55:15 +0100

    Styled Repo Page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,11 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
   return (
     <Container>
-      <h1>Repo</h1>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/joaogil"}>
+          joaogil
+        </Link>
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/joaogil/github-react-project"}>
+          Github-React-Project
+        </Link>
+      </Breadcrumb>
+
+      <p>A Github clone developed with ReactJS </p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/JoaoGil06/Github-React-Project"}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
     </Container>
   );
 };
